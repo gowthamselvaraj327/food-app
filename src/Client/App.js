ac73c09bf5a8c9edc7628bda2361b07430e7912c
GOWTHAM S <gowthamselvaraj327@gmail.com>
2023-03-01 18:54:17 +0530

    Update App.js

--- src/Client/App.js
+++ src/Client/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-    <Route path='/' element={<Login/>}/>
+    <Route path='food-app/' element={<Login/>}/>
     <Route path='/signup' element={<Signup/>}/>
     <Route path='/restaurant' element={<Data/>}/>
     </Routes>
